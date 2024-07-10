3a529ef9ef60cd207f6788d9e4304c4bf8ae7e15
Guillermo Fernández <guillefergom@gmail.com>
2024-07-10 11:11:00 +0200

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
+import {CommonModule } from "@angular/common";
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-app';
+  title = 'Hola Mundosss Angular';
+  enabled: boolean = false;
+
+  courses: string[] = ['Angular', 'React' , 'SpringBoot']
+
+
+  setEnabled(): void{
+    this.enabled = this.enabled? false : true;
+    console.log('Hemos hecho click en setEnabled ');
+  }
 }
